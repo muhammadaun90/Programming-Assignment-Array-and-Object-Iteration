5f0ebc46832aeec021d7d75e35daf7ce8039c601
muhammadaun90 <muhammadaun_90@hotmail.com>
2024-01-19 20:40:32 +0500

    Task1 created

---
+++ app.js
@@ -0,0 +1,11 @@
+// Task 1
+var dairy = ['cheese', 'sour cream', 'milk', 'yogurt', 'ice cream', 'milkshake'];
+function logDairy() {
+    for (let key of dairy) {
+        console.log("Dairy:", key);
+    }
+}
+logDairy();
+// Task 2
+
+// Task 3
