263b4b303407b98914b43cb9e16ae0d94566451d
muhammadaun90 <muhammadaun_90@hotmail.com>
2024-01-19 21:00:23 +0500

    Task2 is completed

--- app.js
+++ app.js
@@ -7,5 +7,16 @@ function logDairy() {
 }
 logDairy();
 // Task 2
-
+const animal = {
+    canJump: true,
+};
+const bird = Object.create(animal);
+bird.canFly = true;
+bird.hasFeathers = true;
+function birdCan() {
+    for (const key of Object.keys(bird)) {
+        console.log(key, ":", bird[key]);
+    }
+}
+birdCan();
 // Task 3
